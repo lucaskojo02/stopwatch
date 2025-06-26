69cec304a7119fe6fc549944b605708a5dccc7bd
lucaskojo02 <lucaskojo02@gmail.com>
2025-06-26 16:36:26 +0300

    Completed

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -40,20 +40,30 @@ function Stopwatch(){
 
     function formatTime(){
 
-        return `00:00:00`;
+        let hours = Math.floor(elapsedTime/(1000*60*60));
+        let minutes = Math.floor(elapsedTime/(1000*60)%60);
+        let seconds =Math.floor (elapsedTime/(1000)%60)
+        let milliseconds = Math.floor((elapsedTime % 1000)/10)
+
+        hours = String(hours).padStart(2,"0")
+        minutes = String(minutes).padStart(2,"0")
+        seconds = String(seconds).padStart(2,"0")
+        milliseconds = String(milliseconds).padStart(2,"0");
+
+        return `${hours}:${minutes}:${seconds}.${milliseconds}`;
     }
 
     return(
         <div  className="stopwatch">
             <div className="display">{formatTime()}</div>
             <div className="controls">
-                <button className="start-button" onClick={start()}>
+                <button className="start-button" onClick={start}>
                     Start
                 </button>
-                <button className="stop-button" onClick={stop()}>
+                <button className="stop-button" onClick={stop}>
                     Stop
                 </button>
-                <button className="reset-button" onClick={reset()}>
+                <button className="reset-button" onClick={reset}>
                     Reset
                 </button>
             </div>
